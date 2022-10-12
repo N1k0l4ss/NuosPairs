595b0cb8b0103d5c3fd7d89ecca58b1bcee17d1c
N1k0l4ss <nikita777white@gmail.com>
2022-10-12 13:49:50 +0300

    Add refreshing nearest subjects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {Subject} from "./service/subjects/subject";
 import {HttpErrorResponse} from "@angular/common/http";
 import {SubjectService} from "./service/subjects/subject.service";
+import {group} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
@@ -15,11 +16,13 @@ export class AppComponent {
   public subjects: Subject[] = [];
   public nearTimingSubjects: Subject[] = [];
   public isNextWeek = false;
+  private firstInit = true;
 
     public static apiServiceUrl = 'https://spring-pairs.herokuapp.com/pairs';
 // public static apiServiceUrl = 'http://localhost:8080/pairs';
 
   constructor(private subjectService: SubjectService) {
+      this.nearestSubjectsWatcher();
   }
 
   public fillSubjects() {
@@ -27,8 +30,25 @@ export class AppComponent {
           return;
 
       this.getSubjectsByParam(this.group, this.query + (this.isNextWeek ? '/next' : ''));
-      this.getNearTimingSubject(this.group);
   }
+
+    async nearestSubjectsWatcher() {
+        if (this.firstInit) {
+            this.firstInit = false;
+            await this.delay(100);
+        }
+        while (true) {
+            console.log("works");
+            if (this.group !== null)
+                this.getNearTimingSubject(this.group);
+            await this.delay(30000);
+        }
+    }
+
+    private delay(ms: number)
+    {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
   private getSubjectsByParam(group: String, param: String): void {
     this.subjectService.getSubjectsByParam(group, param).subscribe(
